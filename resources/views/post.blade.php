6418e73b2777c2fbefee20df4646374677e7950f
MayThuKyaw010618 <scm.maythukyaw@gmail.com>
2021-12-24 13:36:42 +0630

    Section 2 : 08 Store Blog Posts as HTML Files

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,8 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Blog</a></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident amet dolor laborum deleniti eveniet quis unde, molestias ut in consequuntur sapiente aperiam tempore eos facilis debitis enim minus magni nobis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit esse ullam dicta quam praesentium sint a mollitia. Quia, blanditiis. Laboriosam iure quibusdam porro distinctio cum dolorem vero praesentium libero voluptatum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium reiciendis iste dolorem eos nostrum mollitia explicabo ut unde obcaecati excepturi, blanditiis necessitatibus cupiditate natus numquam eius facilis iure odio exercitationem?</p>
+        <?= $post; ?>
     </article>
     <a href="/">Go back</a>
 </body>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,17 +5,17 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Blog</a></h1>
+        <h1><a href="/posts/my-first-post">My First Blog</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident amet dolor laborum deleniti eveniet quis unde, molestias ut in consequuntur sapiente aperiam tempore eos facilis debitis enim minus magni nobis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit esse ullam dicta quam praesentium sint a mollitia. Quia, blanditiis. Laboriosam iure quibusdam porro distinctio cum dolorem vero praesentium libero voluptatum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium reiciendis iste dolorem eos nostrum mollitia explicabo ut unde obcaecati excepturi, blanditiis necessitatibus cupiditate natus numquam eius facilis iure odio exercitationem?</p>
     </article>
 
     <article>
-        <h1><a href="/post">My Second Blog</a></h1>
+        <h1><a href="/posts/my-second-post">My Second Blog</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident amet dolor laborum deleniti eveniet quis unde, molestias ut in consequuntur sapiente aperiam tempore eos facilis debitis enim minus magni nobis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit esse ullam dicta quam praesentium sint a mollitia. Quia, blanditiis. Laboriosam iure quibusdam porro distinctio cum dolorem vero praesentium libero voluptatum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium reiciendis iste dolorem eos nostrum mollitia explicabo ut unde obcaecati excepturi, blanditiis necessitatibus cupiditate natus numquam eius facilis iure odio exercitationem?</p>
     </article>
 
     <article>
-        <h1><a href="/post">My Third Blog</a></h1>
+        <h1><a href="/posts/my-third-post">My Third Blog</a></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident amet dolor laborum deleniti eveniet quis unde, molestias ut in consequuntur sapiente aperiam tempore eos facilis debitis enim minus magni nobis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit esse ullam dicta quam praesentium sint a mollitia. Quia, blanditiis. Laboriosam iure quibusdam porro distinctio cum dolorem vero praesentium libero voluptatum. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium reiciendis iste dolorem eos nostrum mollitia explicabo ut unde obcaecati excepturi, blanditiis necessitatibus cupiditate natus numquam eius facilis iure odio exercitationem?</p>
     </article>
 </body>
