668a79503d49f0e122be2fd4ab55d1caf39f8b98
MayThuKyaw010618 <scm.maythukyaw@gmail.com>
2021-12-27 15:32:01 +0630

    Section 4: 24 Your First Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,9 @@
         {!! $post->title !!} 
       </h1>
       <p>
+        <a href="#">{{ $post->category->name }}</a>
+      </p>
+      <p>
       {!! $post->body !!}
       </p>
     </article>
