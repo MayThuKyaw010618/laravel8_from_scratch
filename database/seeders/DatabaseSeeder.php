13e3210c75b83e89e0354552160359c875996a21
MayThuKyaw010618 <scm.maythukyaw@gmail.com>
2021-12-27 17:03:47 +0630

    Section 4: 27 Database Seeding Saves Time

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,45 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'Work'
+        ]);
+
+        
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'slug' => 'my family post',
+            'title' => 'My Family Post',
+            'excerpt' => 'family',
+            'body' => 'this is body',
+        ]);
+
+         Post::create([
+            'user_id' => $user->id,
+            'category_id' => $family->id,
+            'title' => 'hello',
+            'slug' => 'my_first_blog',
+            'excerpt' => '<p>my excerpt</p>',
+            'body' => '<p>hello,i am here</p>'
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
         {!! $post->title !!} 
       </h1>
       <p>
-        <a href="#">{{ $post->category->name }}</a>
+        By <a href="#">{{ $post->user->name }} </a> in <a href="#">{{ $post->category->name }}</a>
       </p>
       <p>
       {!! $post->body !!}
