3b45347df3faa1e6354d15d505d9a1e895b3a083
MayThuKyaw010618 <scm.maythukyaw@gmail.com>
2021-12-28 21:44:42 +0630

    Section 5: 35 How to ExtractDropdownBladeComponent

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
         'posts' => Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('/posts/{post:slug}', function(Post $post) {
     //find a post by its slug and pass it to a view called "post"
@@ -39,7 +39,7 @@ Route::get('/categories/{category:slug}', function(Category $category) {
         'currentCategory' => $category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('/authors/{author:username}', function(User $author) {
     return view('posts', [
