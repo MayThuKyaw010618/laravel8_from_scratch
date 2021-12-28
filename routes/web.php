fb0802b485a92a7212c5dadc91a715696da6e2a5
MayThuKyaw010618 <scm.maythukyaw@gmail.com>
2021-12-28 09:50:57 +0630

    Section 4: 30 Eager Load Relationships

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Log;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->with(['category','author'])->get()
+        'posts' => Post::latest()->get()
     ]);
 });
 
