7358b93bb1cbcd368b1bb6999a0928a59289d9e5
MayThuKyaw010618 <scm.maythukyaw@gmail.com>
2021-12-28 09:39:13 +0630

    Section 4: 29 View All Posts By An Author

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,21 +1,20 @@
 <x-layout>
-    <article>
-      <h1>
-        {!! $post->title !!} 
-      </h1>
-      <p>
-        By <a href="/authors/{{ $post->author->username }}">
-          {{ $post->author->name }}
-      </a> 
+  <article>
+    <h1>
+      {!! $post->title !!}
+    </h1>
+    <p>
+      By <a href="/authors/{{ $post->author->username }}">
+        {{ $post->author->name }}
+      </a>
       in
       <a href="/categories/{{ $post->category->slug }}">
-          {!! $post->category->name !!}
+        {!! $post->category->name !!}
       </a>
-      </p>
-      <p>
+    </p>
+    <p>
       {!! $post->body !!}
-      </p>
-    </article>
-    <a href="/">Go back</a>
+    </p>
+  </article>
+  <a href="/">Go back</a>
 </x-layout>
-    
