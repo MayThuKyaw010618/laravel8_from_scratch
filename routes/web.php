545d920015b018dc66a875e601aa4550d5ca803e
MayThuKyaw010618 <scm.maythukyaw@gmail.com>
2021-12-29 13:08:53 +0630

    Section 07: 41 Author Filtering

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use App\Models\Category;
-use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,8 +17,3 @@ use App\Models\User;
 Route::get('/', [ PostController::class, 'index' ])->name('home');
 Route::get('posts/{post:slug}', [ PostController::class, 'show' ]);
 
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'posts' => $author->posts
-    ]);
-});
