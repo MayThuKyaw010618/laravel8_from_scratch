20e2eece5356529bae6312ac539ce0b06bc918b4
MayThuKyaw010618 <scm.maythukyaw@gmail.com>
2021-12-30 14:10:28 +0630

    Section 11: 61 Toy Chests and Contracts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
+use App\Services\Newsletter;
+use App\Services\MailchimpNewsletter;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +17,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'server' => 'us20'
+            ]);
+
+            return new MailchimpNewsletter($client);
+        });
     }
 
     /**
