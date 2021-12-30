7a6fd0d890dcda4b7a4323312f373bdd167cdbbd
MayThuKyaw010618 <scm.maythukyaw@gmail.com>
2021-12-30 11:47:20 +0630

    Section 11: 58 Mailchimp API Tinkering

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,11 @@
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script defer src="https://unpkg.com/alpinejs@3.5.0/dist/cdn.min.js"></script>
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
 
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
@@ -17,18 +22,19 @@
 
             <div class="mt-8 md:mt-0 flex items-center">
                 @auth
-                    <span class="text-xs fond-bold uppercase">Welcome {{ auth()->user()->name }}!</span>
+                <span class="text-xs fond-bold uppercase">Welcome {{ auth()->user()->name }}!</span>
 
-                    <form method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6">
-                        @csrf
-                        <button type="submit">Logout</button>
-                    </form>
+                <form method="POST" action="/logout" class="text-xs font-semibold text-blue-500 ml-6">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
                 @else
-                    <a href="/register" class="text-xs font-bold uppercase">Register</a>
-                    <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
+                <a href="/register" class="text-xs font-bold uppercase">Register</a>
+                <a href="/login" class="ml-6 text-xs font-bold uppercase">Log In</a>
                 @endauth
 
-                <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+                <a href="#newsletter"
+                    class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
                 </a>
             </div>
@@ -36,7 +42,8 @@
 
         {{ $slot }}
 
-        <footer class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
+        <footer id="newsletter"
+            class="bg-gray-100 border border-black border-opacity-5 rounded-xl text-center py-16 px-10 mt-16">
             <img src="/images/lary-newsletter-icon.svg" alt="" class="mx-auto -mb-6" style="width: 145px;">
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
             <p class="text-sm mt-3">Promise to keep the inbox clean. No bugs.</p>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
-use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,18 +15,33 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', [ PostController::class, 'index' ])->name('home');
-Route::get('posts/{post:slug}', [ PostController::class, 'show' ]);
+Route::get('ping', function () {
+    //dd(openssl_get_cert_locations());
+    $mailchimp = new \MailchimpMarketing\ApiClient();
 
-Route::post('posts/{post:slug}/comments', [CommentController::class , 'store']);
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us20',
+    ]);
 
+    $response = $mailchimp->lists->addListMember('e1ed97d652', [
+        'email_address' => 'maythukyaw@gmail.com',
+        'status' => 'subscribed',
+    ]);
+    
+    ddd($response);
+});
 
-Route::get('register', [RegisterController::class,'create' ])->middleware('guest');
-Route::post('register', [RegisterController::class,'store' ])->middleware('guest');
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::post('posts/{post:slug}/comments', [CommentController::class, 'store']);
+
+Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
 Route::get('login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionController::class, 'store'])->middleware('guest');
-Route::post('logout', [SessionController::class,'destory' ])->middleware('auth');
-
+Route::post('logout', [SessionController::class, 'destory'])->middleware('auth');
